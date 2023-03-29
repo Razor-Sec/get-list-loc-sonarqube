3b98086f2c8ad9aae6847a9e335e04892bc1c3bc
dumb <dumb@Sprite.mshome.net>
2023-03-29 14:04:36 +0700

    Fixing TypeError: string indices must be integers

--- get_loc.py
+++ get_loc.py
@@ -8,7 +8,7 @@ sonar = SonarQubeClient(sonarqube_url=url, username=username, password=password)
 
 paling_banyak = 0
 total = 0
-project = list(sonar.projects.search_projects())
+project = sonar.projects.search_projects()['components']
 branch = ""
 
 with open("loc.csv", "a") as file_object:
@@ -17,18 +17,14 @@ with open("loc.csv", "a") as file_object:
 		projects = project[i]['key']
 		nama_project = project[i]['name']
 		loc = sonar.project_branches.search_project_branches(project=projects)
-		#print("Project : ", projects, "\nNama : ",nama_project,"\n\n")
 		if len(loc['branches']) > 0 :
 			for j in range(0,len(loc['branches'])):
 				component = sonar.measures.get_component_with_specified_measures(component=projects, branch=loc['branches'][j]['name'],metricKeys="ncloc")
 				branch = loc['branches'][j]['name']
 				if component['component']['measures'] :
-					#print("Branch : " , loc['branches'][j]['name'], "\t:" , component['component']['measures'][0]['value'])
 					if paling_banyak < int(component['component']['measures'][0]['value']) :
 						paling_banyak = int(component['component']['measures'][0]['value'])
 						branch = loc['branches'][j]['name']
-			#print('-'*50)
-			#print("LOC TERBANYAK : ", paling_banyak, "(",branch,")")
 			tes = nama_project+","+projects+","+branch+","+str(paling_banyak)+","
 			print(nama_project+","+projects+","+branch+","+str(paling_banyak)+",")
 			file_object.write(tes)
@@ -37,7 +33,6 @@ with open("loc.csv", "a") as file_object:
 			paling_banyak = 0
 		else:
 			component = sonar.measures.get_component_with_specified_measures(component=projects, branch=loc['branches'][0]['name'],metricKeys="ncloc")
-			#print("Branch : " , loc['branches'][0]['name'], ":".ljust(50) , component['component']['measures'][0]['value'])
 			tess = nama_project+","+projects+","+branch+","+str(paling_banyak)+","
 			print(nama_project+","+projects+","+branch+","+str(paling_banyak)+",")
 			file_object.write(tess)
